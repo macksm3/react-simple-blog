17ac64664ffb3ee7827d3696fd63705a5d5e0d10
MacksM3 <64023042+macksm3@users.noreply.github.com>
2021-05-27 05:15:38 -0400

    first edit of base generated code

---
+++ src/components/DisplayAllPosts.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'React';
+import CreateNewPost from './CreateNewPost'
+const DisplayAllPosts = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const savePostTitleToState = event => {
+    setTitle(event.target.value);
+    console.log(title)
+  };
+  const savePostContentToState = event => {
+    setContent(event.target.value);
+    console.log(content)
+  };
+      return (
+    <>
+    <CreateNewPost 
+    savePostTitleToState = {savePostTitleToState}
+    savePostContentToState = {savePostContentToState}
+    />
+    </>
+    )
+}
+export default DisplayAllPosts
+
